782b1bd5b375ed2d41b0a4bce7bdd806232dcdba
mycognosist <mycognosist@cryptolab.net>
2018-08-31 21:31:30 +0200

    Add ability to store a culture and return all cultures

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ A culture library & cultivation log for tracking the expansion of mycelia. Built
  - Setup minimal kappa-core application / instance  [ x ]
  - Append a JSON 'culture' object to a feed         [ x ]
  - Print 'culture' objects from a feed              [ x ]
- - Write CLI tool with commander                    [ o ]
-   - Add a culture                                  [ o ]
-   - List all cultures                              [ o ]
+ - Write CLI tool with commander                    [ x ]
+   - Add a culture                                  [ x ]
+   - List all cultures                              [ x ]
+   - Add action selector                            [ o ]
+
+## Resources
+
+[inquirer hierarchical.js example](https://github.com/SBoudrias/Inquirer.js/blob/master/packages/inquirer/examples/hierarchical.js)
--- index.js
+++ index.js
@@ -1,37 +1,66 @@
+#!/usr/bin/env node
+
+var inquirer = require('inquirer')
+
 var kappa = require('kappa-core')
 var hypercore = require('hypercore')
 
 var core = kappa('./log', { valueEncoding: 'json' })
 
-var culture = {
-  'genus': 'Ganoderma',
-  'species': 'lucidum',
-  'strain': 'Aleon1',
-  'code': 'GLAL001'
-}
+var questions = [
+  {
+    type: 'input',
+    name: 'code',
+    message: 'Unique ID'
+  },
+  {
+    type: 'input',
+    name: 'genus',
+    message: 'Genus'
+  },{
+    type: 'input',
+    name: 'species',
+    message: 'Species'
+  },{
+    type: 'input',
+    name: 'strain',
+    message: 'Strain'
+  }
+]
 
-var cultureView = {
-  api: {
-    get: function (core, cb) {
-      this.ready(function () {
-        cb(null, culture)
+console.log('            ')
+console.log('[ MYKAFILE ]')
+console.log('            ')
+console.log('Add a Culture:')
+console.log('            ')
+
+inquirer.prompt(questions).then(answers => {
+  
+  var culture = (JSON.stringify(answers, null, ' '));
+
+  var cultureView = {
+    api: {
+      get: function (core, cb) {
+        this.ready(function () {
+          cb(null, culture)
+        })
+      }
+    },
+    map: function (msgs, next) {
+      msgs.forEach(function (msg) {
+	console.log(msg.value);
       })
-    }
-  },
-  map: function (msgs, next) {
-    msgs.forEach(function (msg) {
-      msg.value
+      next()
+    },
+  }
+  
+  core.use('culture', cultureView)
+  
+  core.feed('cultures', function (err, feed) {
+    feed.append(culture, function (err) {
+      core.api.culture.get(function (err, value) {
+        return //1
+      })
     })
-    next()
-  },
-}
-
-core.use('culture', cultureView)
-
-core.feed('cultures', function (err, feed) {
-  feed.append(culture, function (err) {
-    core.api.culture.get(function (err, value) {
-      console.log(value) //1
-    })
-  })
+  });
 })
